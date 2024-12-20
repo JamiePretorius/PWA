9039b6ed8db044086e01507f7112b39788cd5de1
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2024-12-20 16:23:08 +0200

    ci:

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('/index.html').then((response) => {
+        return response || fetch('/index.html');
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
